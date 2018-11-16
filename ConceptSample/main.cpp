e46ed2f7b8fceb257063d52a5e14f80e71c622b8
vmille <vmille@aneo.fr>
2018-11-16 17:30:52 +0100

    adding sample for concept-ts

--- ConceptSample/main.cpp
+++ ConceptSample/main.cpp
@@ -1,10 +1,4 @@
-#include "ConceptExpressions_2.h"
-
-#include <vector>
 #include <iostream>
-#include <tuple>
-#include <cassert>
-#include <functional>
 
 template <typename T>
 concept bool cBase = true;
@@ -51,26 +45,9 @@ struct Wrapper<T> {
 };
 
 int main() {
-
     Wrapper<Derived1> d1;
     d1.display();
 
     Wrapper<Derived2> d2;
     d2.display();
-
-//    double tab[3] = {1.0, 2.0, 3.0};
-//    std::vector<double> v1{4.0, 5.0, 6.0};
-//    std::vector<double> v2{7.0, 8.0, 9.0};
-
-//    Toto<3> tab{1.0, 2.0, 3.0};
-//    Toto<3> v1{4.0, 5.0, 6.0};
-//    Toto<3> v2{7.0, 8.0, 9.0};
-//
-//    apply<Streamer>(tab+v1+v2, std::cout);
-//    auto op{tab+v1+v2};
-//    auto result{op};
-
-//    for (int i = 0; i < 3; ++i) {
-//        std::cout << "result[" << i << "] : " << result[i] << std::endl;
-//    }
 }
